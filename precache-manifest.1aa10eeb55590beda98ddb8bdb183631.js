d11dc33aeb58c54db82f18cb8675d8f39db886c7
Samuel <babo.samuel0@gmail.com>
2019-11-05 17:22:27 +0100

    Deploy

---
+++ precache-manifest.1aa10eeb55590beda98ddb8bdb183631.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9aea3ab65b5b7965e1aa",
+    "url": "/js/chunk-vendors.c7dafd34.js"
+  },
+  {
+    "revision": "c78bc706bb9fbeab34dc",
+    "url": "/js/app.6813d1f5.js"
+  },
+  {
+    "revision": "8eac6d761f84451ad852",
+    "url": "/js/about.c2288143.js"
+  },
+  {
+    "revision": "a28d03bf2dd6dc17e3c7e8fad9c07321",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9aea3ab65b5b7965e1aa",
+    "url": "/css/chunk-vendors.7981fadf.css"
+  },
+  {
+    "revision": "c78bc706bb9fbeab34dc",
+    "url": "/css/app.4d521594.css"
+  }
+];
